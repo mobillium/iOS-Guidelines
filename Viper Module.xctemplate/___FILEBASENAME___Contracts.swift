24da7df35bbc4495b483b85e4cfe7f33dde6a0da
Turgay AFŞAR <afsarturgay@gmail.com>
2019-12-06 14:02:11 +0300

    VIPER module XCode template created

---
+++ Viper Module.xctemplate/___FILEBASENAME___Contracts.swift
@@ -0,0 +1,31 @@
+//___FILEHEADER___
+
+import Foundation
+
+// MARK: - View
+
+protocol ___VARIABLE_moduleName___ViewProtocol: class {
+    
+}
+
+// MARK: - Presenter
+
+protocol ___VARIABLE_moduleName___PresenterProtocol: class {
+    
+}
+
+// MARK: - Interactor
+
+protocol ___VARIABLE_moduleName___InteractorProtocol: class {
+    var delegate: ___VARIABLE_moduleName___InteractorDelegate? { get set }
+}
+
+protocol ___VARIABLE_moduleName___InteractorDelegate: class {
+    
+}
+
+// MARK: - Router
+
+protocol ___VARIABLE_moduleName___RouterProtocol: class {
+    
+}
