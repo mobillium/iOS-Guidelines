a08b3f2ebfbd04a6ff42aa3e2a8b5867a487f148
Turgay AFŞAR <afsarturgay@gmail.com>
2019-12-25 13:24:04 +0300

    VIPER template import bash script created

---
+++ Template/Source/VIPER Module.xctemplate/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,9 @@
+//___FILEHEADER___
+
+import Foundation
+
+final class ___VARIABLE_moduleName___Interactor: ___VARIABLE_moduleName___InteractorProtocol {
+    
+    weak var delegate: ___VARIABLE_moduleName___InteractorDelegate?
+    
+}
---
+++ Template/import_viper_template
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+cp -r Source/* "$HOME/Library/Developer/Xcode/Templates/"
