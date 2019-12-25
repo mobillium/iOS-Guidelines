0017a3d2236f238cab7d69a4cd0df398dc37b59d
Turgay AFŞAR <afsarturgay@gmail.com>
2019-12-25 13:27:41 +0300

    Removed other templates

---
+++ Templates/Source/VIPER Module.xctemplate/___FILEBASENAME___Router.swift
@@ -0,0 +1,13 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___VARIABLE_moduleName___Router: ___VARIABLE_moduleName___RouterProtocol {
+    
+    unowned let view: UIViewController
+    
+    init(view: UIViewController) {
+        self.view = view
+    }
+}
+
---
+++ Templates/Source/VIPER Module.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,13 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___VARIABLE_moduleName___ViewController: UIViewController, ___VARIABLE_moduleName___ViewProtocol {
+    
+    var presenter: ___VARIABLE_moduleName___PresenterProtocol?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
