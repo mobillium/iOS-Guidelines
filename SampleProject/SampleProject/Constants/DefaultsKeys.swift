18b87f3ab415c211032cc897bf2d46f0b3099b86
Mehmet Salih Aslan <aslanmsalih@gmail.com>
2021-02-05 12:55:22 +0300

    add MSADateFormatter and MSAUserDefaults libraries.

---
+++ SampleProject/SampleProject/Constants/DefaultsKeys.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultsKeys.swift
+//  SampleProject
+//
+//  Created by Mehmet Salih Aslan on 5.02.2021.
+//  Copyright © 2021 Mobillium. All rights reserved.
+//
+
+import MSAUserDefaults
+
+extension DefaultsKey {
+    static let stringKey = Key<String>(key: "stringKey")
+}
