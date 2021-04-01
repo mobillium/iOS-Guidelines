f356115595641dd78f9bde096e4fe667740312d8
Mehmet Salih Aslan <aslanmsalih@gmail.com>
2021-04-01 14:25:48 +0300

    add TableViewDelegate and CollectionViewDelegate options to MVVM-R Scene template.

---
+++ Templates/Source/Mobillium Templates/MVVM-R Scene.xctemplate/CollectionViewDelegate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,47 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___VARIABLE_moduleName___ViewController: BaseViewController<___VARIABLE_moduleName___ViewModel> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+// MARK: - UICollectionViewDataSource
+extension ___VARIABLE_moduleName___ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+}
+
+//swiftlint:disable line_length
+// MARK: - UICollectionViewDelegateFlowLayout
+extension ___VARIABLE_moduleName___ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        <#code#>
+    }
+    
+}
+//swiftlint:enable line_length
---
+++ Templates/Source/Mobillium Templates/MVVM-R Scene.xctemplate/TableViewDelegate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,23 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___VARIABLE_moduleName___ViewController: BaseViewController<___VARIABLE_moduleName___ViewModel> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+// MARK: - UITableViewDataSource
+extension ___VARIABLE_moduleName___ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
