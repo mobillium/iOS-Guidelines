463a078058dd750eda38508d25a9233309e2da17
Mehmet Salih Aslan <aslanmsalih@gmail.com>
2022-09-15 16:52:11 +0300

    init project

---
+++ SampleProjectSwiftUI/SampleProjectSwiftUI/SampleProjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectSwiftUIApp.swift
+//  SampleProjectSwiftUI
+//
+//  Created by Mehmet Salih Aslan on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
